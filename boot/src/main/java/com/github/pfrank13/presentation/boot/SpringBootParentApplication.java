35091d5d966fd5343d3c64eff4b75606e7bd1aea
Peter Frank <peter.frank@mobileforming.com>
2016-11-09 11:29:23 -0800

    Initial commit

---
+++ boot/src/main/java/com/github/pfrank13/presentation/boot/SpringBootParentApplication.java
@@ -0,0 +1,12 @@
+package com.github.pfrank13.presentation.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootParentApplication.class, args);
+	}
+}
