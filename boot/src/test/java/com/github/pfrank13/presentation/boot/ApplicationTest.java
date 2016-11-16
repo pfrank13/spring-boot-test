4219f8b197d77ed60d22928415d2a52bff428389
Peter Frank <pfrank13@gmail.com>
2016-11-15 18:58:53 -0800

    End 2 End test setup with mock services
    Right now we have unintialized (outside of DB SChema) and a mock service to facilitate an end 2 end style test against fake services

--- boot/src/test/java/com/github/pfrank13/presentation/boot/ApplicationTest.java
+++ boot/src/test/java/com/github/pfrank13/presentation/boot/ApplicationTest.java
@@ -3,7 +3,10 @@ package com.github.pfrank13.presentation.boot;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.core.AutoConfigureCache;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureMockRestServiceServer;
@@ -12,15 +15,16 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.client.MockRestServiceServer;
 
-import javax.persistence.PersistenceContext;
-
 /**
  * @author pfrank
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockRestServiceServer
-@DataJpaTest
+@AutoConfigureCache
+@AutoConfigureDataJpa
+@AutoConfigureTestDatabase
+@AutoConfigureTestEntityManager
 public class ApplicationTest {
   @Autowired
   private TestRestTemplate testRestTemplate;
